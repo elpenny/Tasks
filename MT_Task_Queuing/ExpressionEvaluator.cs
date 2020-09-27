ec4cff0d83695db27bf8bda92c95ff0874bb08c5
Mikolaj <mikolaj.kubista@gmail.com>
2020-09-27 18:02:26 +0200

    Dependencies of Task Producer added

---
+++ MT_Task_Queuing/ExpressionEvaluator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MT_Task_Queuing
+{
+    internal class ExpressionEvaluator
+    {
+        public string Anaylyze(string expression)
+        {
+            expression.Split();
+            return expression;
+        }
+    }
+}
