95c57881e839e510378dffc1d27fb87569c0c1e0
Mikolaj <mikolaj.kubista@gmail.com>
2020-09-27 18:38:44 +0200

    ExpressionGenerator stub

---
+++ MT_Task_Queuing/Services/ExpressionGenerator.cs
@@ -0,0 +1,15 @@
+﻿using MT_Task_Queuing.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MT_Task_Queuing.Services
+{
+    internal class ExpressionGenerator : IExpressionGenerator
+    {
+        public string GenerateExpression()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
